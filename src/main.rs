4a2b7ff68a85364c153bafa8608f4ceba609c6fb
patrikduch <duchpatrik@icloud.com>
2025-03-24 23:31:27 +0100

    feat:  server preparation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 mod controllers;
 mod models;
 mod routes;
+use log::{info, warn, error};
+use dotenv::dotenv;
 
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server at http://127.0.0.1:8080");
+    println!("Starting server at http://127.0.0.1:80");
+
+    dotenv().ok(); // Load environment variables
+    env_logger::init(); // Initialize logging
     
     HttpServer::new(|| {
         App::new()
             .configure(routes::configure_routes)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:80")?
     .run()
     .await
 }
